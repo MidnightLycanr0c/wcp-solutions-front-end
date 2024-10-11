17854630cbe7182f47f4eeb2636b5f04e9059515
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-10-11 12:05:48 -0500

    add(): navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <header
+        class="w-full p-4 flex justify-start items-center bg-black"
+    >
+        <div class="w-auto h-full flex items-center justify-center text-white">
+            <a
+                class="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto"
+                href="/"
+            >
+                <span class="sr-only">WCP Solutions Home Page</span>
+                <img  class="w-auto h-5" src="../assets/wcp-logo.svg" alt="WCP Solutions Logo" >
+            </a>
+        </div>
+    </header>
+</template>
