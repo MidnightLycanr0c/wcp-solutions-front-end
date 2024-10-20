fdca424cc9c7de43c517a13ddf880e97e9027612
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-10-19 23:21:18 -0500

    add(dev): path aliases to the project for cleaner imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import path from 'node:path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@':            path.resolve(__dirname, 'src'),
+            '@/assets':     path.resolve(__dirname, 'src/assets'),
+            '@/components': path.resolve(__dirname, 'src/components'),
+            '@/router':     path.resolve(__dirname, 'src/router'),
+        }
+    }
 })
