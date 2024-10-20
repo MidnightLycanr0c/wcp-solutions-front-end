9ecc32d70a31e01191b6de002ecb7c65ea5bc837
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-10-20 02:09:08 -0500

    add(): views path alias

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             '@/assets':     path.resolve(__dirname, 'src/assets'),
             '@/components': path.resolve(__dirname, 'src/components'),
             '@/router':     path.resolve(__dirname, 'src/router'),
+            '@/views':      path.resolve(__dirname, 'src/views'),
         }
     }
 })
