20d58f28bc28f1559ba2f3faeeb3fa677fd34017
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-10-20 18:31:51 -0500

    refactor(): homepage link and current year in the footer

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,13 @@
 <template>
     <header class="w-full py-6 flex justify-start items-center bg-black">
         <div class="w-auto h-full flex items-center justify-center text-white">
-            <a class="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto" href="/">
+            <RouterLink
+                to="/"
+                class="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto"
+            >
                 <span class="sr-only">WCP Solutions Home Page</span>
                 <img class="w-auto h-7" :src="WCPWebLogo" alt="WCP Solutions Logo" >
-            </a>
+            </RouterLink>
         </div>
     </header>
 
@@ -12,12 +15,19 @@
 
     <footer class="w-full p-4 flex justify-start items-center bg-black">
         <div class="w-auto h-full flex text-white flex-col">
-            <span class="text-xs mt-0">© 2024 WCP Solutions, All Rights Reserved.</span>
+            <span class="text-xs mt-0">
+                &copy; {{ current_year }}
+                WCP Solutions, All Rights Reserved.
+            </span>
         </div>
     </footer>
 </template>
 
 
 <script setup lang="ts">
-import { WCPWebLogo } from '@/assets';
+import { ref } from "vue";
+
+import { WCPWebLogo } from "@/assets";
+
+const current_year = ref(new Date().getFullYear());
 </script>
