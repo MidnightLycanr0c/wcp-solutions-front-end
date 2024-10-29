1f2e51f0bfc18835683bdc51cde84acf07d74411
austin-mel <austin.csus@gmail.com>
2024-10-29 14:15:33 -0700

    #TBWPCSOL-86 add(): Logout button to navigate back to login page

--- src/views/DashboardView/DashboardView.vue
+++ src/views/DashboardView/DashboardView.vue
@@ -58,6 +58,7 @@ const showAcountManagerColumn = computed(() => {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
+                stroke-width="1.5"
                 stroke="currentColor"
                 class="w-10 h-10 text-black"
             >
@@ -65,7 +66,25 @@ const showAcountManagerColumn = computed(() => {
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M5.121 18.364A9 9 0 1112 21a9 9 0 01-6.879-2.636zM15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+            </svg>
+        </RouterLink>
+
+        <!-- Inserting logout icon using Heroicons-->
+        <!-- TODO: WE PROBABLY WANT TO MOVE THIS ICON TO THE HEADER-->
+        <RouterLink to="/" class="ml-4">
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="#de1a34" 
+                class="w-10 h-10 text-black">
+                <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" 
                 />
             </svg>
         </RouterLink>
